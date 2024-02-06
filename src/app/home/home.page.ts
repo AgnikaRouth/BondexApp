023ae978d8ec34513d4ed30081de7d480a6f8fb4
Agnika Routh <agnika.routh@gmail.com>
2024-02-07 01:35:29 +0530

    feat:setup Swiper

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   constructor() {}
 
+  // ionViewDidEnter() {
+  //   // Initialize Swiper when the view has entered
+  //   var mySwiper = new Swiper('.swiper-container', {
+  //     direction: 'horizontal',
+  //     loop: true,
+  //     pagination: {
+  //       el: '.swiper-pagination',
+  //     },
+  //   });
+  // }
+
+  ionViewDidEnter() {
+    // Initialize Swiper when the view has entered
+    var swiper = new Swiper('.mySwiper', {
+      slidesPerView: 3,
+      // centeredSlides: true,
+      loop: true,
+      direction: 'horizontal',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+    });
+  }
 }
